b49a28432b815636f15f9c5f4573864219490f2d
Alejandra Evander <alejandra.evander92@gmail.com>
2024-02-21 17:06:08 +0800

    Second Commit_21022024

--- conftest.py
+++ conftest.py
@@ -1,76 +1,12 @@
-# import pytest
-# from selenium import webdriver
-# from selenium.webdriver.edge.service import Service
-# #from selenium.webdriver.firefox.service import Service
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.wait import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-
-# driver = None
-
-# def pytest_addoption(parser):
-#     parser.addoption(
-#         "--browser_name", action="store", default="edge"
-#     )
-
-# @pytest.fixture(scope="class")
-# def setup(request):
-#     global driver
-#     browser_name = request.config.getoption("browser_name")
-#     if browser_name == "edge":
-#         # options_obj = webdriver.EdgeOptions()
-#         # options_obj.add_argument("--guest")
-#         # options_obj.add_experimental_option('excludeSwitched', ['enable-logging'])
-#         # service_obj = Service(r"H:\Python_Project\Python_Selenium_Framework\Drivers\edgedriver_win64\msedgedriver.exe") 
-#         # driver = webdriver.Edge(service=service_obj, options=options_obj)
-        
-#         options_obj = webdriver.EdgeOptions()
-#         options_obj.add_argument("--guest")
-#         #options_obj.add_argument("windows-size=1920x1080")
-#         #Code to disable devtools listening on ....
-#         options_obj.add_experimental_option('excludeSwitches', ['enable-logging'])
-#         #options_obj.add_argument("--headless=new")
-#         #ptions_obj.add_argument("--start-maximized")
-#         #options_obj.add_argument("headless")
-#         #options_obj.add_argument("--ignore-certificate-errors")
-#         service_obj = Service(r"H:\Python_Project\Section_9_RS_Testing_Framework\edgedriver_win64\msedgedriver.exe")
-#         driver = webdriver.Edge(service=service_obj, options=options_obj)
-        
-    
-#     elif browser_name == "firefox":
-#         options_obj = webdriver.FirefoxOptions()
-#         options_obj.add_argument("--guest")
-#         options_obj.add_experimental_option('excludeSwitched', ['enable-logging'])
-#         service_obj = Service(r"H:\Python_Project\Python_Selenium_Framework\Drivers\geckodriver-v0.34.0-win-aarch64\geckodriver.exe") 
-#         driver = webdriver.Firefox(service=service_obj, options=options_obj)
-    
-#     elif browser_name == "chrome":
-#         options_obj = webdriver.ChromeOptions()
-#         options_obj.add_argument("--guest")
-#         options_obj.add_experimental_option('excludeSwitched', ['enable-logging'])
-#         service_obj = Service(r"H:\Python_Project\Python_Selenium_Framework\Drivers\chromedriver-win64\chromedriver.exe") 
-#         driver = webdriver.Chrome(service=service_obj, options=options_obj)
-     
-     
-#     #Start Website using the one of the browser chosen
-#     driver.implicitly_wait(10)
-#     driver.get("https://papamy.panasonic.com.my/papamy/")
-#     driver.maximize_window()
-#     request.cls.driver=driver
-#     yield
-#     driver.close()
-
 import pytest
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service
+#from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver import ActionChains
-import time
+from selenium.webdriver.support import expected_conditions as EC
 
-driver =  None
+driver = None
 
 def pytest_addoption(parser):
     parser.addoption(
@@ -80,8 +16,14 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="class")
 def setup(request):
     global driver
-    browser_name=request.config.getoption("browser_name")
-    if browser_name=="edge":
+    browser_name = request.config.getoption("browser_name")
+    if browser_name == "edge":
+        # options_obj = webdriver.EdgeOptions()
+        # options_obj.add_argument("--guest")
+        # options_obj.add_experimental_option('excludeSwitched', ['enable-logging'])
+        # service_obj = Service(r"H:\Python_Project\Python_Selenium_Framework\Drivers\edgedriver_win64\msedgedriver.exe") 
+        # driver = webdriver.Edge(service=service_obj, options=options_obj)
+        
         options_obj = webdriver.EdgeOptions()
         options_obj.add_argument("--guest")
         #options_obj.add_argument("windows-size=1920x1080")
@@ -93,16 +35,26 @@ def setup(request):
         #options_obj.add_argument("--ignore-certificate-errors")
         service_obj = Service(r"H:\Python_Project\Section_9_RS_Testing_Framework\edgedriver_win64\msedgedriver.exe")
         driver = webdriver.Edge(service=service_obj, options=options_obj)
-
-      
-       
+        
+    
     elif browser_name == "firefox":
-        options_obj = webdriver.EdgeOptions()
-        service_obj = Service(r"H:\Python_Project\Section_9_RS_Testing_Framework\edgedriver_win64\msedgedriver.exe")
+        options_obj = webdriver.FirefoxOptions()
+        options_obj.add_argument("--guest")
+        options_obj.add_experimental_option('excludeSwitched', ['enable-logging'])
+        service_obj = Service(r"H:\Python_Project\Python_Selenium_Framework\Drivers\geckodriver-v0.34.0-win-aarch64\geckodriver.exe") 
         driver = webdriver.Firefox(service=service_obj, options=options_obj)
-
+    
+    elif browser_name == "chrome":
+        options_obj = webdriver.ChromeOptions()
+        options_obj.add_argument("--guest")
+        options_obj.add_experimental_option('excludeSwitched', ['enable-logging'])
+        service_obj = Service(r"H:\Python_Project\Python_Selenium_Framework\Drivers\chromedriver-win64\chromedriver.exe") 
+        driver = webdriver.Chrome(service=service_obj, options=options_obj)
+     
+     
+    #Start Website using the one of the browser chosen
     driver.implicitly_wait(10)
-    driver.get("https://rahulshettyacademy.com/angularpractice/")
+    driver.get("https://papamy.panasonic.com.my/papamy/")
     driver.maximize_window()
     request.cls.driver=driver
     yield
@@ -117,7 +69,7 @@ def pytest_runtest_makereport(item):
     pytest_html = item.config.pluginmanager.getplugin('html')
     outcome = yield
     report = outcome.get_result()
-    extra = getattr(report, 'extra', [])
+    extra = getattr(report, 'extras', [])
 
     if report.when == 'call' or report.when == "setup":
         xfail = hasattr(report, 'wasxfail')
--- test_page.py
+++ test_page.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.by import By
@@ -10,23 +11,28 @@ from Common_Startup.BaseClass import BaseClass
 class TestHomepage(BaseClass):
     def test_menu(self):
         
+        #Clicking Home Button & Checking whether correct or not
         self.driver.find_element(By.XPATH,"//a[@class='s1']").click()
-        Home_Text = self.driver.find_element(By.XPATH,"span[@class='bold']").text
-        assert "Home" in Home_Text
+        Home_Text = self.driver.find_element(By.XPATH,"//span[@class='bold']").text
+        #print(Home_Text)
+        assert "HOME" in Home_Text
         
+        #Clicking About Us Button & Checking whether correct or not
         self.driver.find_element(By.XPATH,"//a[@class='s2']").click()
-        AboutUs_Text = self.driver.find_element(By.XPATH,"span[@class='bold']").text
+        AboutUs_Text = self.driver.find_element(By.XPATH,"//span[@class='bold']").text
         assert "About PAPAMY" in AboutUs_Text
         
+        #Clicking Product -> AC Button & Checking whether correct or not
+        self.driver.find_element(By.XPATH,"//a[@class='s3']").click()
         action = ActionChains(self.driver)
-        action.move_to_element(self.driver.find_element(By.LINK_TEXT,"Products")).perform()
-        action.move_to_element(self.driver.find_element(By.LINK_TEXT,"Air Conditioners")).click().perform()
-        AC_Text = self.driver.find_element(By.XPATH,"span[@class='bold']").text
+        action.move_to_element(self.driver.find_element(By.XPATH,"//a[@href='modelrange.asp']")).click().perform()
+        AC_Text = self.driver.find_element(By.XPATH,"//span[@class='bold']").text
         assert "Products / Model Range" in AC_Text
         
-        action.move_to_element(self.driver.find_element(By.LINK_TEXT,"Products")).perform()
-        action.move_to_element(self.driver.find_element(By.LINK_TEXT,"Compressors")).click().perform()
-        Compressors_Text = self.driver.find_element(By.XPATH,"span[@class='bold']").text
+        #Clicking Product -> Compressor & Checking whether correct or not
+        self.driver.find_element(By.XPATH,"//a[@class='s3']").click()
+        action.move_to_element(self.driver.find_element(By.XPATH,"//a[@href='compressor.asp']")).click().perform()
+        Compressors_Text = self.driver.find_element(By.XPATH,"//span[@class='bold']").text
         assert "Products / Compressor / Rotary Compressor" in Compressors_Text
         
         
